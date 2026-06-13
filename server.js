931ba3074a4c0babe3b99fef94f6a59883571c88
Parth Surve <surveparth429@gmail.com>
2026-06-13 15:19:57 +0530

    chore: initialize expense tracker project

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/health', (req, res) => {
+  res.json({
+    Health: 'OK',
+    Status: 'Expense Tracker API running...'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Expense Tracker API running on http://[::1]:${PORT}`)
+})
